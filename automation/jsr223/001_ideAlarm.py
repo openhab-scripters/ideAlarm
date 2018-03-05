90d0fcc8b85245f85d065e89b782047edcaab2fa
Richard Roe <36725726+besynnerlig@users.noreply.github.com>
2018-03-05 08:08:17 +0100

    Create 001_ideAlarm.py

---
+++ automation/jsr223/001_ideAlarm.py
@@ -0,0 +1,16 @@
+from openhab.rules import rule, addRule
+from idealarm import ideAlarm #######
+from mylib.utils import hasReloadFinished
+
+@rule
+class ideAlarmTrigger(object):
+
+    """Make ideAlarm trigger on item changes"""
+    def getEventTriggers(self):
+        return ideAlarm.getTriggers()
+
+    def execute(self, modules, inputs):
+        if not hasReloadFinished(True): return
+        ideAlarm.execute(modules, inputs)
+
+addRule(ideAlarmTrigger())
